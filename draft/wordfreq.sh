84419ee0d6fe3c611cc91df3840aec39ca2a3522
Nart Tlisha <daniel.abzakh@gmail.com>
2022-05-24 22:35:40 +0300

    Added txt

--- draft/wordfreq.sh
+++ draft/wordfreq.sh
@@ -1,3 +1,3 @@
-sed -r 's/(.*)/\L\1/g' en.txt | sed -r "s/'|[ ]+/ /g" | sed -e "s/[[:punct:][:digit:]]//g" -e '/^$/d' | \
+sed -r 's/(.*)/\L\1/g' $1 | sed -r "s/'|[ ]+/ /g" | sed -r "s/'-/ /g" | sed -r "s/[^[:alpha:] ]//g" | sed -r '/^$/d' | \
 awk '{print tolower($0)}' | tr ' ' '\n'| LC_ALL=C sort | uniq -c | \
-LC_ALL=C sort -nr | sed -r 's/^[ ]+//g' | sed -r '/^[0-9] $/d'> wordfreq-en
+LC_ALL=C sort -nr | sed -r 's/^[ ]+//g' | sed -r '/^[0-9] $/d'> wordfreq-en-${1/.txt/}
